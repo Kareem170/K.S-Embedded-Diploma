467ae2cdc9704893096d92c01eb489e86b4929cf
Kareem170 <kareemmohamed68990@gmail.com>
2024-08-17 18:09:07 +0300

    Assignment1 Solution

---
+++ Unit2/Assignment1/Problem2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int input ;
+    printf("Enter an integer: ");
+    fflush(stdout);
+    scanf("%d",&input);
+    printf("You Entered: %d",input);
+    return 0;
+}
---
+++ Unit2/Assignment1/Problem3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1,num2 ;
+    printf("Enter two integer: ");
+    fflush(stdout);
+    scanf("%d %d",&num1,&num2);
+    printf("Sum: %d",num1+num2);
+    return 0;
+}
